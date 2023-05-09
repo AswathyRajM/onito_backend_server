d7f1e039a7adfa38a0c37ea2e208e0ab4d1c43c5
AswathyRajM <aswathyraj767@gmail.com>
2023-05-09 15:29:39 -0400

    test api setup completed

--- app.ts
+++ app.ts
@@ -9,11 +9,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// catch 404 and forward to error handler
-app.use(function (req: Request, res: Response, next: any) {
-  next(createError(404));
-});
-
 const allowedMethods = ['GET', 'POST', 'PUT', 'PATCH'];
 app.use((req, res, next) => {
   if (!allowedMethods.includes(req.method))
--- routes/movie.routes.ts
+++ routes/movie.routes.ts
@@ -2,8 +2,8 @@ import express from 'express';
 // import movieController from '../controllers/movie.contoller.js';
 
 const router = express.Router();
-router.post('/test', (req, res) => {
-  res.send('Its a test API for portraying route name for bulk add');
+router.get('/test', (req, res) => {
+  res.send('Testing..');
 });
 // router.post('/add', validate(bikeSchema), bikeController.addBikeRecord);
 // router.get('/getById/:id', validate(bikeSchemaId), bikeController.findBike);
