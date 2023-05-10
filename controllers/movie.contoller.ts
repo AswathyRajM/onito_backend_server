f982ab64ee5a6e07186a25609aa0c2f5ef167068
AswathyRajM <aswathyraj767@gmail.com>
2023-05-10 17:02:08 -0400

    top-rated-api and create api added

--- controllers/movie.contoller.ts
+++ controllers/movie.contoller.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from 'express';
+import { Op, Sequelize } from 'sequelize';
 import Movie from '../models/movie';
+import Rating from '../models/ratings';
 class MovieController {
   static getLongestDurationMovies = async (req: Request, res: Response) => {
     Movie.findAll({
       limit: 10,
-      order: ['runtimeMinutes', 'DESC'],
+      order: [['runtimeMinutes', 'DESC']],
       attributes: [
         'tconst',
         'titletype',
@@ -27,31 +29,58 @@ class MovieController {
       });
   };
 
-  static findBike = (req: any, res: any) => {
-    const id = req.params.id;
+  static addMovie = (req: any, res: any) => {
+    const { tconst, titletype, primaryTitle, runtimeMinutes, genres } =
+      req.body;
+    Movie.create({ tconst, titletype, primaryTitle, runtimeMinutes, genres })
+      .then((data: any) => {
+        if (data) {
+          res.send('success');
+        }
+      })
+      .catch((err: any) => {
+        res.status(500).send({
+          resultCode: 0,
+          message: err.message || 'Server Error',
+          error_code: '500',
+        });
+      });
   };
 
-  static findAllBike = (req: any, res: any) => {
+  static getTopRatedMovies = (req: any, res: any) => {
     const id = req.params.id;
-    // Bike.findAllBikes()
-    //   .then((data: any) => {
-    //     if (data) {
-    //       res.send(data);
-    //     } else {
-    //       res.status(404).send({
-    //         resultCode: 0,
-    //         message: 'Bike not found',
-    //         error_code: '404',
-    //       });
-    //     }
-    //   })
-    //   .catch((err: any) => {
-    //     res.status(500).send({
-    //       resultCode: 0,
-    //       message: err.message || 'Server Error',
-    //       error_code: '400',
-    //     });
-    //   });
+    Rating.findAll({
+      where: {
+        averageRating: {
+          [Op.gt]: 6.0,
+        },
+      },
+      order: [['averageRating', 'DESC']],
+      attributes: [
+        'tconst',
+        'averageRating',
+        [Sequelize.col('movie.primaryTitle'), 'primaryTitle'],
+        [Sequelize.col('movie.genres'), 'genres'],
+      ],
+      include: {
+        model: Movie,
+        as: 'movie',
+        attributes: [],
+      },
+      raw: true,
+    })
+      .then((data: any) => {
+        if (data) {
+          res.send(data);
+        }
+      })
+      .catch((err: any) => {
+        res.status(500).send({
+          resultCode: 0,
+          message: err.message || 'Server Error',
+          error_code: '500',
+        });
+      });
   };
 }
 
--- routes/movie.routes.ts
+++ routes/movie.routes.ts
@@ -3,8 +3,11 @@ import movieController from '../controllers/movie.contoller';
 
 const router = express.Router();
 
-router.get('/longest-duration-movies', movieController.getLongestDurationMovies);
-// router.get('/getById/:id', validate(bikeSchemaId), bikeController.findBike);
-// router.get('/getAll', bikeController.findAllBike);
+router.get(
+  '/longest-duration-movies',
+  movieController.getLongestDurationMovies
+);
+router.post('/new-movie', movieController.addMovie);
+router.get('/top-rated-movies', movieController.getTopRatedMovies);
 
 export { router };
